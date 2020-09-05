13d008b8504669666d786fd2124686ba2e4322e7
Youngseok Hahm <terryhahm@gmail.com>
2020-09-05 13:27:56 +0900

    scroll snap

--- src/Home.js
+++ src/Home.js
@@ -1,116 +1,67 @@
 import React, { Component, useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 
 function Home() {
 
-    const scrollToRef = (ref) => window.scrollTo({ 
-        behavior: 'smooth', 
-        top: ref.current.offsetTop
-    })   
-    const useMountEffect = (fun) => useEffect(fun, [])
-
-    const introRef = useRef()    
-    const aboutRef = useRef()
-    const projectRef = useRef()
-    const reviewRef = useRef()
-    const postRef = useRef()
-
-    // const refLoc = [introRef.current.offsetTop, aboutRef.current.offsetTop, projectRef.current.offsetTop, reviewRef.current.offsetTop, postRef.current.offsetTop ]
-
-    // const [refIdx, setRefIdx] = useState(0) 
-
-    useMountEffect(() => scrollToRef(introRef)) // Scroll on mount
-
-
-    const prevScrollY = useRef(0);
-
-    const [refIdx, setRefIdx] = useState(0);
-    const [goingUp, setGoingUp] = useState(false);
-    const [goingDown, setGoingDown] = useState(false);
-
-    const [atRef, setAtRef] = useState(true);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollY = window.scrollY;
-            if (prevScrollY.current < currentScrollY) {
-                // setGoingUp( false );
-                setGoingDown( true); 
-            }
-            if (prevScrollY.current > currentScrollY) {
-                setGoingUp( true);
-                // setGoingDown( false); 
-            }
-            prevScrollY.current = currentScrollY;
-            console.log( goingUp, goingDown )
-            
-        };
-
-        window.addEventListener("scroll", handleScroll );
-        setGoingUp( false );
-        setGoingDown( false); 
-
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, [goingUp, goingDown]);
+ 
 
     return (
-        <Layout>
-            <Container ref = {introRef}>
+        <Container>
+            <Page id ="intro" >
                 <Panel>
                     <Title>
                         <h2> Youngseok Terry Hahm / 함영석 </h2>
                     </Title>
                     <LinkContainer> 
-                        <Btn onClick={()=>scrollToRef(aboutRef)} >
+                        <Btn href = '#about' >
                             About
                         </Btn>
-                        <Btn onClick={()=>scrollToRef(projectRef)}>
+                        <Btn href = '#project'>
                             Projects
                         </Btn>
-                        <Btn onClick={()=>scrollToRef(reviewRef)}>
+                        <Btn href = '#review'>
                             Reviews
                         </Btn>
-                        <Btn onClick={()=>scrollToRef(postRef)}>
+                        <Btn href = '#post'>
                             Posts
                         </Btn>
                     </LinkContainer>
                 </Panel>
-            </Container>
-            <Container ref = {aboutRef}>
-             
-                    <h2> About Me Part</h2>
-                
-            </Container>
-            <Container ref = {projectRef}>
-    
-                    <h2> Project Part</h2>
-            </Container>
-            <Container ref = {reviewRef}>
-                    <h2> Reviews Part</h2>
-            </Container>
-            <Container ref = {postRef}>
-                    <h2> Posts Part</h2>
-            </Container>
-
-
-        </Layout>
+            </Page>
+            <Page id ="about" >
+                <h2> About Me Part</h2>
+            </Page>
+            <Page id ="project" >
+                <h2> Project Part</h2>
+            </Page>
+            <Page id ="review" >
+                <h2> Reviews Part</h2>
+            </Page>
+            <Page id ="post" >
+                <h2> Posts Part</h2>
+            </Page>
+        </Container>
     );
 }
 
 
-const Layout = styled.div`
-  flex: 1;
-  flex-direction: column;
+const Container = styled.div`
+    flex: 1;
+    flex-direction: column;
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
 `
 
-
-const Container = styled.div`
+const Page = styled.div`
     display: flex;
     flex: 1;
     justify-content: center;
     align-items: center;
     height: 100vh;
+    scroll-snap-align: start;
+    
 `;
 
 const Panel = styled.div`
@@ -121,6 +72,7 @@ const Panel = styled.div`
     height: 50%;
     box-shadow: 5px 5px 1px #606d61;
     background-color : #c6d2be;
+
 `;
 
 const Title = styled.div`
@@ -141,7 +93,7 @@ const LinkContainer = styled.div`
     height: 30%;
 `;
 
-const Btn = styled.button`
+const Btn = styled.a`
     display: flex;
     width: 20%;
     height: 100%;
